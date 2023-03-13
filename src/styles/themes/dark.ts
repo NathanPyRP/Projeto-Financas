b367431d4ef7c594856d276db511f9d79923e700
Dev1 <nathanquirino123@gmail.com>
2023-03-13 14:37:16 -0300

    finance commit

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,17 @@
+export default {
+    title: 'dark',
+
+    colors: {
+        primary: '#263238',
+        secondary: '#37474f',
+        tertiary: '#455a64',
+
+        white: '#FFF',
+        black: '#000',
+        gray: '#BFBFBF',
+
+        success: '#4E41F0',
+        info: '#F7931B',
+        warning: '#c62828',
+    },
+};
